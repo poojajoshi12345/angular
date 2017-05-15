171f940643e4b098657f225efb2cf7d5002cbd0f
Dave_Marom <>
2017-05-15 19:49:32 +0000

    [BIP-3407] Modify the CSS on the toolbar buttons

--- src/main/resources/META-INF/resources/ibx/resources/js/collapsible.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/collapsible.ibx.js
@@ -73,7 +73,24 @@ $.widget("ibi.ibxCollapsible", $.Widget,
 	},
 	toggle: function ()
 	{
-		(this.isOpen()) ? this.close() : this.open();
+		if (this.isOpen())
+		{
+			if (this.element.hasClass("pd-left-pane"))
+				$("#toggleleft").removeClass("pd-toolbar-button-active");
+			else
+				if (this.element.hasClass("pd-right-pane"))
+					$("#toggleright").removeClass("pd-toolbar-button-active");				
+			this.close();
+		}
+		else
+		{
+			if (this.element.hasClass("pd-left-pane"))
+				$("#toggleleft").addClass("pd-toolbar-button-active");
+			else
+				if (this.element.hasClass("pd-right-pane"))
+					$("#toggleright").addClass("pd-toolbar-button-active");				
+			this.open();
+		} 
 	},
 	_onWindowMouseEvent: function (e)
 	{
