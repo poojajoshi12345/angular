bed3d16dca383c1ee84aee6479f47833e597b498
Julian_Hyman <>
2017-08-21 20:28:34 +0000

    better context menu handling.

--- src/main/resources/META-INF/resources/ibx/resources/js/widget.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/widget.ibx.js
@@ -137,8 +137,7 @@ $.widget("ibi.ibxWidget", $.Widget,
 		ctxMenu = ctxEvent.result || $(this.options.ctxMenu);
 		if(ctxMenu.length)
 		{
-			ctxMenu.ibxWidget("option", "position", {my:"", at:""});
-			ctxMenu.offset({top:e.clientY, left:e.clientX});
+			ctxMenu.ibxWidget("option", "position", {my:"left top", at:"", collision:"fit", of:e});
 			ctxMenu.ibxWidget("open");
 			e.stopPropagation();
 			e.preventDefault();
