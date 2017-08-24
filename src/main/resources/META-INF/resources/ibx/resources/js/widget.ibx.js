d6f4ebb614a20f868175116749618d555a303a1a
Julian_Hyman <>
2017-08-24 03:44:46 +0000

    Had a bug where refresh was being called from option when requestion value and not setting...bounty for Telu!

--- src/main/resources/META-INF/resources/ibx/resources/js/widget.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/widget.ibx.js
@@ -170,7 +170,7 @@ $.widget("ibi.ibxWidget", $.Widget,
 	},
 	option:function(key, value)
 	{
-		var bRefresh = (typeof(key) == "object") || (this.options[key] != value);
+		var bRefresh = (typeof(key) == "object") || (value !== undefined && (this.options[key] != value));
 		var ret = null;
 		if(value !== undefined)
 			ret = this._super(key, value);
