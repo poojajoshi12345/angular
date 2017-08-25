6d72b05a8be3c3c5e11c84df62c9a71fc3bbccf2
Allen_Williams <>
2017-08-25 21:44:21 +0000

    finished ibxtree virtual list

---
+++ src/test/resources/META-INF/resources/ibx/testing/allen/tree_revised/resources/js/implementXml.js
@@ -0,0 +1,122 @@
+/**
+ * Created by aw14286 on 8/24/2017.
+ */
+function xmlDataNode() {
+  ibxDataNode.apply(this, arguments);
+}
+
+xmlDataNode.prototype = Object.create(ibxDataNode.prototype, {
+  'constructor': xmlDataNode
+});
+
+/**
+ * [getChildren description]
+ * @return {[type]} [description]
+ */
+xmlDataNode.prototype.getChildren = function() {
+  "THIS FUNCTION GODDAMN WORKED"
+  var $dfd = $.Deferred();
+  if (this._children == null || this._children.length == 0) {
+    var tmp = this._children = $.map($(this._data)[0]
+      .children,
+      function(val, index) {
+        return val.id != "" ? val.id : val.innerHtml;
+      })
+    console.log(tmp)
+    console.log("now i'm gonna check")
+    $dfd.resolve(tmp);
+
+  } else {
+
+    console.log("now i'm gonna check - sike!!!")
+    $dfd.resolve(this._children);
+  }
+  return $dfd.promise().done(function(val) {
+    console.log("will I ever get here?");
+    console.log(val);
+    return val
+  });
+};
+xmlDataNode.prototype.hasChildren = function() {
+  try {
+    return $(this._data)[0]
+      .children.length > 0;
+  } catch (e) {
+  }
+};
+xmlDataNode.prototype._setIndexFromData = function(data) {
+  if ($(data)
+    .attr("id")) {
+    return $(data)
+      .attr("id")
+  } else {
+   return $(data)[0].attr("id")
+  }
+
+};
+
+xmlDataNode.prototype.getDescription = function() {
+  return this.index;
+}
+
+/**
+ * example function demonstrating how to build concrete implementation on top of abstract data provider
+ * @constructor
+ */
+function xmlDataProvider() {
+  ibxDataProvider.apply(this, arguments);
+  var reader = new FileReader();
+  this.getReader = function() {
+    return reader
+  }
+  $('#file-input')
+    .on('change', this.parseFileData.bind(this))
+}
+xmlDataProvider.prototype = Object.create(ibxDataProvider.prototype, {
+  'constructor': xmlDataProvider
+});
+
+xmlDataProvider.prototype.init = function() {
+
+  this._showNodes(this.cachedData, null)
+}
+xmlDataProvider.prototype.parseFileData = function(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var self = this;
+  this.getReader().onload = function(e) {
+    this.contents = e.target.result;
+
+    this.elem = $.parseXML(this.contents);
+    this.rootElem = $(this.elem)
+      .children();
+
+    $.each(this.rootElem.children(), function(index, val) {
+      this.addDataNode(val, null);
+    }.bind(this));
+    //debugger;
+
+    $.each(this.cachedData, function(index, val) {
+      this._visibleList.push(val.index);
+    }.bind(this))
+
+    // initialize the root node
+    console.log(this._visibleList.length)
+  }.bind(this);
+  this.getReader()
+    .readAsText(file);
+}
+
+xmlDataProvider.prototype.isCorrectType = function(toTest){
+  if(typeof toTest == 'object'){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+xmlDataProvider.prototype.getDataFromDoc = function(str){
+ return this.rootElem.children().find("*[id="+str+"]")[0]
+}
