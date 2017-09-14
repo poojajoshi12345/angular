18e666e39169e68925f0dd2796f3c001d6b1643a
Allen_Williams <>
2017-09-14 20:11:53 +0000

    wai aria examples (minus keyboard navigation)

--- src/test/resources/META-INF/resources/ibx/testing/allen/wai-aria-examples/js/tabs.js
+++ src/test/resources/META-INF/resources/ibx/testing/allen/wai-aria-examples/js/tabs.js
@@ -26,32 +26,73 @@
    };
 
    // Bind listeners
-   for (i = 0; i < tabs.length; ++i) {
+   for (i = 0; i < tabs.length; ++i)
+   {
       addListeners(i);
-   };
+   }
+   ;
 
-   function addListeners (index) {
-      console.log("adding listeners")
+   function addListeners(index)
+   {
       tabs[index].addEventListener('click', clickEventListener);
-
+      //tabs[index].addEventListener('keydown', keydownEventListener);
+      tabs[index].addEventListener('LEFT', left);
+      tabs[index].addEventListener('RIGHT', right);
       // Build an array with all tabs (<button>s) in it
       tabs[index].index = index;
    };
 
-   function clickEventListener (event) {
-      console.log("been clicked")
+   function clickEventListener(event)
+   {
       var tab = event.target;
       activateTab(tab, false);
    };
 
+   function left(e)
+   {
+      console.log("detected the left keypress on ")
+      console.log(e.target)
+      var tabIndex = Array.from(tabs)
+                          .indexOf(e.target);
+      var tab;
+      if (tabIndex == 0)
+      {
+         tab = tabs[tabs.length - 1];
+      } else
+      {
+         tab = tabs[tabIndex - 1];
+      }
+      activateTab(tab, true);
+   }
+
+   function right(event)
+   {
+      console.log("detected the right keypress on ")
+      console.log(event.target)
+      var tabIndex = Array.from(tabs)
+                          .indexOf(event.target);
+      var tab;
+
+      if (tabIndex === tabs.length - 1)
+      {
+         tab = tabs[0];
+      } else
+      {
+         tab = tabs[tabIndex + 1]
+      }
+      activateTab(tab, true);
+   }
+
    // Activates any given tab panel
-   function activateTab (tab, setFocus) {
+   function activateTab(tab, setFocus)
+   {
       setFocus = setFocus || true;
       // Deactivate all other tabs
       deactivateTabs();
 
       // Remove tabindex attribute
-      tab.removeAttribute('tabindex');
+      //tab.removeAttribute('tabindex');
+      tab.setAttribute('tabindex', '0');
 
       // Set the tab as selected
       tab.setAttribute('aria-selected', 'true');
@@ -60,24 +101,32 @@
       var controls = tab.getAttribute('aria-controls');
 
       // Remove hidden attribute from tab panel to make it visible
-      document.getElementById(controls).setAttribute('aria-hidden', "false");
+      document.getElementById(controls)
+              .setAttribute('aria-hidden', "false");
 
       // Set focus when required
-      if (setFocus) {
+      if (setFocus)
+      {
          tab.focus();
-      };
+      }
+      ;
    };
 
-   function deactivateTabs () {
-      for (t = 0; t < tabs.length; t++) {
+   function deactivateTabs()
+   {
+      for (t = 0; t < tabs.length; t++)
+      {
          tabs[t].setAttribute('tabindex', '-1');
          tabs[t].setAttribute('aria-selected', 'false');
          //tabs[t].removeEventListener('focus', focusEventHandler);
-      };
+      }
+      ;
 
-      for (p = 0; p < panels.length; p++) {
+      for (p = 0; p < panels.length; p++)
+      {
          panels[p].setAttribute('aria-hidden', 'true');
-      };
+      }
+      ;
    };
 
 })();
