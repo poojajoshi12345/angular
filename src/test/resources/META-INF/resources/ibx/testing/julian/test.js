ac361fbd80ccb3989ee2183b8450eabe0b981100
Julian_Hyman <>
2017-10-05 19:21:25 +0000

    *** empty log message ***

---
+++ src/test/resources/META-INF/resources/ibx/testing/julian/test.js
@@ -0,0 +1,36 @@
+
+
+
+
+		<!--
+		<div tabIndex="0" data-ibx-type="ibxTabPane">
+			<div data-ibx-type="ibxTabPage">Tab 1
+				<div>
+					I'm the tab 1 content!
+				</div>
+			</div>
+			<div data-ibx-type="ibxTabPage">Tab 2
+				<div>
+					I'm the tab2  content!
+				</div>
+			</div>
+		</div>
+		<div tabIndex="0" class="test-menu-button" data-ibx-type="ibxMenuButton">Test Menu
+			<div class="test-menu" data-ibx-type="ibxMenu">
+			</div>
+		</div>
+
+		<div class="parent-key-root" tabIndex="0" data-ibx-type="ibxHBox" data-ibxp-nav-key-root="true" data-ibxp-nav-key-auto-focus="false" data-ibxp-align="center" data-ibxp-justify="center">
+			<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+			<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+
+			<div class="child-key-root" tabIndex="-1" data-ibx-type="ibxHBox" data-ibxp-nav-key-root="true" data-ibxp-nav-key-auto-focus="true" data-ibxp-align="center" data-ibxp-justify="center">
+				<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+				<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+				<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+			</div>
+
+			<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+			<div class="child" tabIndex="-1" data-ibx-type="ibxLabel">Child1</div>
+		</div>
+		-->
