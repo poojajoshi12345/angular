b19922bc49a3ebc46d76e738a0d28a499a35b629
Julian_Hyman <>
2018-02-13 23:39:36 +0000

    [PD-695] code cleanup.

--- src/main/resources/META-INF/resources/ibx/resources/js/tabs.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/tabs.ibx.js
@@ -154,7 +154,6 @@ $.widget("ibi.ibxTabPane", $.ibi.ibxFlexBox,
 		}
 
 		this._tabBar.css("flex", "0 0 auto");
-		//this._tabBar.on("ibx_change", this._onTabChange.bind(this)).attr("tabIndex", -1);
 		this.element.prepend(this._tabBar);
 	},
 	_removeTabBar: function ()
