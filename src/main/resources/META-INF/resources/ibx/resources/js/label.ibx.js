e2dc5c4d096284d2c52211ed0df4ed01e895f4ec
Julian_Hyman <>
2018-03-17 12:23:55 +0000

    [IBX-100] make ibxLabels work like regular html labes with 'for'...click will focus target control

--- src/main/resources/META-INF/resources/ibx/resources/js/label.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/label.ibx.js
@@ -18,6 +18,8 @@ $.widget("ibi.ibxLabel", $.ibi.ibxFlexBox,
 		"glyphElClass":"ibx-label-glyph",
 		"glyphElSpacerClass":"ibx-glyph-spacer",
 
+		"for":null, //make labels work like <label>...focus target on click.
+
 		/*label overlays...array of objects: {"position":"xx", "glyph":"xx", "glyphClasses":"xx", "icon":"xx", "iconClasses":"xx"}*/
 		"overlays":[],
 
@@ -35,6 +37,8 @@ $.widget("ibi.ibxLabel", $.ibi.ibxFlexBox,
 	{
 		this._super();
 		var options = this.options;
+
+		this.element.on("click", this._onLabelClick.bind(this));
 
 		//alternate to data-ibxp-text...direct text node children can be used to set the text.
 		options.text = options.text || this.element.textNodes().remove().text().replace(/^\s*|\s*$/g, "");
@@ -55,6 +59,12 @@ $.widget("ibi.ibxLabel", $.ibi.ibxFlexBox,
 		this._glyph.remove();
 		this._text.remove();
 		this.element.removeClass("icon-left icon-top icon-right icon-bottom")
+	},
+	_onLabelClick:function(e)
+	{
+		var options = this.options;
+		if(options.for)
+			$(options.for).focus();
 	},
 	_refresh:function()
 	{
