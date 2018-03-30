de2ce15abfe0d2053152a1350a9bc471de36dfe2
Allen_Williams <>
2018-03-30 18:43:48 +0000

    [BIP-3142] build un-minified slp.jsp

--- src/test/resources/META-INF/resources/ibx/testing/allen/mv/gulpfile.js
+++ src/test/resources/META-INF/resources/ibx/testing/allen/mv/gulpfile.js
@@ -12,15 +12,12 @@ gulp.task('inline', function ()
    return gulp.src('./src/index.html')
               .pipe(inline({
                  base: 'src/',
-                 js: uglify,
+                 // js: uglify, /*uncomment to minify the js*/
                  css: minifyCss
               }))
               .pipe(replace("<!--JSPHEAD-->", fs.readFileSync('./src/templates/head.jsp', 'utf8')))
-              //.pipe(replace("<!--IBXINIT-->", '<jsp:include page="/WEB-INF/jsp/ibx/ibxInit.jsp" flush="false"/>'))
               .pipe(rename('slp.jsp'))
               .pipe(gulp.dest('./build'))
-              .pipe(gulp.dest('C:/ibi/WebFOCUS82/webapps/webfocus/slp'))
-              .pipe(gulp.dest('C:/ibi/WebFOCUS82/webapps/webfocus/WEB-INF/jsp/slp'))
 });
 
 gulp.task('default', ['inline']);
