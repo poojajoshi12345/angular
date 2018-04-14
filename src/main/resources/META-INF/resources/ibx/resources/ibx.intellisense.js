a3a6177fe2b1b61bca1dd21fd2cce3a0f2100a58
Julian_Hyman <>
2018-04-14 13:17:03 +0000

    *** empty log message ***

--- src/main/resources/META-INF/resources/ibx/resources/ibx.intellisense.js
+++ src/main/resources/META-INF/resources/ibx/resources/ibx.intellisense.js
@@ -1,21 +1,11 @@
 /*Copyright 1996-2016 Information Builders, Inc. All rights reserved.*/
 // $Revision$:
 
+/****
+	THIS IS A PLACEHOLDER FILE FOR WHEN I GET AROUND TO ADDING Microsoft Visual Studio INTELLISENSE SUPPORT TO IBX
+****/
 intellisense.addEventListener("statementcompletion", function(e)
 {
-/*
-    intellisense.logMessage(  
-        e.target ? 'member list requested, target: ' + e.targetName : 'statement completion for current scope requested');  
-
-	
-
-    // Prints out all statement completion items.  
-    e.items.forEach(function(item)
-	{  
-        intellisense.logMessage('[completion item] ' + item.name + ', kind:' + item.kind + ', scope:' + item.scope + ', value:' + item.value);  
-    });  
-*/
-
 });
 
 intellisense.addEventListener("statementcompletionhint", function(e)
