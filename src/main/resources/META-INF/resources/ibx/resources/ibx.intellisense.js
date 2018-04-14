a836ae3e2a14d7b8d67450f423e5839bddd47747
Julian_Hyman <>
2018-04-14 13:07:05 +0000

    just adding a placeholder for possible Microsoft Visual Studio intellisense support.

---
+++ src/main/resources/META-INF/resources/ibx/resources/ibx.intellisense.js
@@ -0,0 +1,28 @@
+/*Copyright 1996-2016 Information Builders, Inc. All rights reserved.*/
+// $Revision$:
+
+intellisense.addEventListener("statementcompletion", function(e)
+{
+/*
+    intellisense.logMessage(  
+        e.target ? 'member list requested, target: ' + e.targetName : 'statement completion for current scope requested');  
+
+	
+
+    // Prints out all statement completion items.  
+    e.items.forEach(function(item)
+	{  
+        intellisense.logMessage('[completion item] ' + item.name + ', kind:' + item.kind + ', scope:' + item.scope + ', value:' + item.value);  
+    });  
+*/
+
+});
+
+intellisense.addEventListener("statementcompletionhint", function(e)
+{
+
+});
+
+intellisense.addEventListener("signaturehelp", function(e)
+{
+});
