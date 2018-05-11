d22c5d554c4e32194b78518d0347d1e79772cc00
Julian_Hyman <>
2018-05-11 13:53:22 +0000

    [IBX-135] ibxLabel - focus the 'for' target on label focus, not click.

--- src/main/resources/META-INF/resources/ibx/resources/js/label.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/label.ibx.js
@@ -38,7 +38,7 @@ $.widget("ibi.ibxLabel", $.ibi.ibxFlexBox,
 		this._super();
 		var options = this.options;
 
-		this.element.on("click", this._onLabelClick.bind(this));
+		this.element.on("focus", this._onLabelFocus.bind(this));
 
 		//alternate to data-ibxp-text...direct text node children can be used to set the text.
 		options.text = options.text || this.element.textNodes().remove().text().replace(/^\s*|\s*$/g, "");
@@ -61,7 +61,7 @@ $.widget("ibi.ibxLabel", $.ibi.ibxFlexBox,
 		this._text.remove();
 		this.element.removeClass("icon-left icon-top icon-right icon-bottom")
 	},
-	_onLabelClick:function(e)
+	_onLabelFocus:function(e)
 	{
 		var options = this.options;
 		if(options.for)
