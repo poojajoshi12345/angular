b359355d8d4a8f70634f6ece30f467a23ba8065e
Telu_Dumitru <>
2019-01-14 21:56:19 +0000

    [CICD-19] Remove onbeforeunload after each test ends, othewise chrome might hang in headless mode.

--- src/test/resources/cypress/support/index.js
+++ src/test/resources/cypress/support/index.js
@@ -18,3 +18,6 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+afterEach(() => { 
+    cy.window().then(win => win.onbeforeunload = undefined );
+});
