e5e6b130b5d02d98b4019c99d91dd496605651eb
Julian_Hyman <>
2019-02-12 22:42:52 +0000

    [IBX-313] ibxNavManager - added navMap/navMapItem for menus support.

--- src/main/resources/META-INF/resources/ibx/resources/js/navmap.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/navmap.ibx.js
@@ -1,6 +1,11 @@
 /*Copyright 1996-2016 Information Builders, Inc. All rights reserved.*/
 // $Revision$:
 
+//sub classes of menu things to use for navigation mapping
+$.widget("ibi.ibxNavMap", $.ibi.ibxMenu,{_widgetClass: "ibx-nav-map", options:{"focusDefault":false, "focusResetOnBlur":true, "refocusLastActiveOnClose":false, "aria":{"label":"Quick Navigation Map"}}});
+$.widget("ibi.ibxNavMapItem", $.ibi.ibxMenuItem,{_widgetClass: "ibx-nav-map-item", options:{}});
+
+//just listen for events to trigger the navigation management
 function ibxNavManager()
 {
 	
@@ -16,28 +21,12 @@ ibxNavManager._onKeyEvent = function(e)
 	if(eventMatchesShortcut(ibxNavManager.options.triggerKey, e))
 	{
 		var target = $(e.target);
-		var navMenu = $("<div>").ibxMenu({"refocusLastActiveOnClose":false});
-		var navRoot = target.parents("[data-ibx-nav-root=true]").first();
-		var navPoints = navRoot.find("[data-ibx-nav-point]");
-		navPoints.each(function(idx, navPoint)
+		var navRoot = target.closest("[data-ibx-nav-map]");
+		var navMap = $(navRoot.attr("data-ibx-nav-map")).ibxWidget("open").on("ibx_select", function(e)
 		{
-			navPoint = $(navPoint);
-			if(navRoot.is(navPoint.parents("[data-ibx-nav-root=true]").first()))
-			{
-				var navInfo = "(" + navPoint.data("ibxNavPoint") + ")";
-				navInfo = eval(navInfo);
-				navInfo.el = navPoint[0];
-				var menuItem = $("<div>").ibxMenuItem({labelOptions:{text:navInfo.label}, userValue:navInfo});
-				navMenu.ibxWidget("add", menuItem);
-			}
-		}.bind(this))
-
-		navMenu.ibxWidget("open").on("ibx_select", function(e)
-		{
-			var navInfo = $(e.originalEvent.target).ibxWidget("option", "userValue");
-			navInfo.el.focus();
-			console.log(navInfo.el);
-		});
+			var navItem = e.originalEvent.target.getAttribute("data-ibx-nav-target");
+			$(navItem).focus();
+		}).focus();
 	}
 }
 
