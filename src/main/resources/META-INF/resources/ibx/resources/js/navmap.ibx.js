dd8af7a8ffafbad9f3a26de06e88eaf90aa98527
Julian_Hyman <>
2019-02-14 21:12:04 +0000

    [IBX-313] ibxNavManager - working on parent navigation.

--- src/main/resources/META-INF/resources/ibx/resources/js/navmap.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/navmap.ibx.js
@@ -2,8 +2,50 @@
 // $Revision$:
 
 //sub classes of menu things to use for navigation mapping
-$.widget("ibi.ibxNavMap", $.ibi.ibxMenu,{_widgetClass: "ibx-nav-map", options:{"focusDefault":false, "focusResetOnBlur":true, "refocusLastActiveOnClose":false, "aria":{"label":"Quick Navigation Map"}}});
-$.widget("ibi.ibxNavMapItem", $.ibi.ibxMenuItem,{_widgetClass: "ibx-nav-map-item", options:{}});
+$.widget("ibi.ibxNavMapItem", $.ibi.ibxMenuItem,{options:{navTarget:null, aria:{role:"listitem"}}, _widgetClass: "ibx-nav-map-item"});
+$.widget("ibi.ibxNavMap", $.ibi.ibxMenu,
+{
+	options:
+	{
+		"navParent":null,
+		"navTarget":null,
+		"focusDefault":false,
+		"focusResetOnBlur":true,
+		"refocusLastActiveOnClose":false,
+		"aria":
+		{
+			"role":"list",
+			"label":ibx.resourceMgr.getString("IBX_NAV_MAP_LABEL")
+		}
+	},
+	_widgetClass: "ibx-nav-map",
+	_create:function()
+	{
+		this._super();
+		this._sep = $("<div>").ibxMenuSeparator();
+		this._navToParent = $("<div>").ibxNavMapItem({labelOptions:{text:ibx.resourceMgr.getString("IBX_NAV_MAP_UP_ON_LEVEL")}});
+	},
+	_onMenuItemClick:function(e)
+	{
+		if(this._navToParent.is(e.target))
+		{
+			$(this.options.navParent).ibxWidget("open");
+		}
+		this._super(e);
+	},
+	_refresh:function()
+	{
+		var options = this.options;
+		this._super();
+		if(options.navParent)
+			this._box.prepend(this._navToParent, this._sep);
+		else
+		{
+			this._navToParent.detach();
+			this._sep.detach();
+		}
+	}
+});
 
 //just listen for events to trigger the navigation management
 function ibxNavManager()
@@ -22,15 +64,21 @@ ibxNavManager._onKeyEvent = function(e)
 	{
 		var target = $(e.target);
 		var navRoot = target.closest("[data-ibx-nav-map]");
-		var navMap = $(navRoot.attr("data-ibx-nav-map")).ibxWidget("open").off("ibx_select").on("ibx_select", function(e)
-		{
-			var navItem = e.originalEvent.target.getAttribute("data-ibx-nav-target");
-			$(navItem).focus();
-		}).focus();
+		var navMap = $(navRoot.attr("data-ibx-nav-map")).ibxWidget("open").off("ibx_select").on("ibx_select", ibxNavManager._onNavItemSelected.bind(this));
 
-
+		var navParent = navRoot.parents("[data-ibx-nav-map]").first();
+		navMap.ibxWidget("option", "navParent", navParent.attr("data-ibx-nav-map"));
 	}
-}
+};
+
+ibxNavManager._onNavItemSelected = function(e)
+{
+	// var navItem = $(e.originalEvent.target);
+	// var navTarget = navItem.ibxWidget("option", "navTarget");
+	// //var navParent = navTarget.ibxWidget()
+	// $(navTarget).focus();
+	// console.log(navTarget);
+};
 
 ibx.navManager = new ibxNavManager();
 
