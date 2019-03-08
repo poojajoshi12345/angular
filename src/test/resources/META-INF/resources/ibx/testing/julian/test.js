649fe18afa64fb5bb5dcde93fd2608710b4f063e
Julian_Hyman <>
2019-03-08 19:26:00 +0000

    *** empty log message ***

--- src/test/resources/META-INF/resources/ibx/testing/julian/test.js
+++ src/test/resources/META-INF/resources/ibx/testing/julian/test.js
@@ -1,9 +1,10 @@
-$(".ibx-tree").css("overflow", "auto").ibxWidget("remove");
 
-var tree = $(".ibx-tree.wfc-mdfp-dimension-tree");
-for(var i = 0; i < 100; ++i)
+//$(".ibxtool-toolbar, .ides-pane-container, .ides-tool-canvas-cp-box, .top-splitter, .measure-tree-box").remove()
+var tree = $(".wfc-mdfp-dimension-tree").css({"overflow":"auto", "fontSize":"1em"}).ibxWidget("remove").ibxWidget("destroy");
+var items = [];
+for(var i = 0; i < 50; ++i)
 {
 	var item = $("<div>item</div>").ibxTreeNode();
-	//tree.ibxWidget("add", item);
+	items.push(item[0]);
 }
-
+tree.ibxWidget("add", items);
