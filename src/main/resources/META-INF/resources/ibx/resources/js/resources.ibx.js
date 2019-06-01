3826450093b4d5cd5d55aade872aad227c17b670
Julian Hyman <julian_hyman@ibi.com>
2019-06-01 14:32:12 -0400

    ibx-388 saved resource style blocks to res cache.

--- src/main/resources/META-INF/resources/ibx/resources/js/resources.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/resources.ibx.js
@@ -1,5 +1,4 @@
 /*Copyright 1996-2016 Information Builders, Inc. All rights reserved.*/
-// $Revision: 1.123 $:
 
 /******************************************************************************
 	RESOURCE BUNDLE MANAGEMENT
@@ -7,7 +6,7 @@
 function ibxResourceManager(ctxPath)
 {
 	if(_jsDerivingClass)return;
-	this._resBundle = $($.parseXML("<ibx-res-bundle><markup></markup></ibx-res-bundle>"));
+	this._resBundle = $($.parseXML("<ibx-res-bundle><styles></styles><markup></markup></ibx-res-bundle>"));
 	this._styleSheet = $("<style type='text/css'>").ibxAddClass("ibxResourceManager_inline_styles").appendTo("head");
 	
 	this.loadedBundles = {};
@@ -336,6 +335,9 @@ _p.loadBundle = function(xResDoc)
 				var styleNode = $("<style type='text/css'>").attr("data-ibx-src", src).text(content);
 				head.append(styleNode);
 				$(window).dispatchEvent("ibx_ibxresmgr", {"hint":"cssinlineloaded", "loadDepth":this._loadDepth, "resMgr":this, "bundle":bundle[0]});
+				
+				styleBlock = styleBlock.clone().text(content);
+				this._resBundle.find("ibx-res-bundle > styles").first().append(styleBlock);//save sheet in res document
 			}
 		}.bind(this));
 
