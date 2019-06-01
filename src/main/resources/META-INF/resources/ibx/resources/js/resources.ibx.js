d705d6567db159e152aeacc8743202bcb8e698ad
Julian Hyman <julian_hyman@ibi.com>
2019-06-01 14:18:59 -0400

    removed revision count

--- src/main/resources/META-INF/resources/ibx/resources/js/resources.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/resources.ibx.js
@@ -1,5 +1,4 @@
 /*Copyright 1996-2016 Information Builders, Inc. All rights reserved.*/
-// $Revision: 1.123 $:
 
 /******************************************************************************
 	RESOURCE BUNDLE MANAGEMENT
