0b566652bd1fcb11b038d44b6ef02951a4cd9312
Julian Hyman <julian_hyman@ibi.com>
2019-06-06 14:59:47 -0400

    fixed stagecvs.bat to do recursive copy

--- stagecvs.bat
+++ stagecvs.bat
@@ -1,5 +1,4 @@
 REM copies files from git to 82xx folder for cvs return
 SET targetDir=C:\ibi\WebFOCUS82\ibx_82xx
 SET sourceDir=%CD%\src\main\resources\META-INF\resources\ibx
-copy %sourceDir%\resources\js\*.js %targetDir%\resources\js\
-copy %sourceDir%\resources\css\*.css %targetDir%\resources\css\
+xcopy %sourceDir%\resources\*.* %targetDir%\resources\*.* /s /e /y
