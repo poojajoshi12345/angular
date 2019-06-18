cca2586e0dd6326370bb3de48253ec66608ae8da
Julian Hyman <julian_hyman@ibi.com>
2019-06-18 17:42:03 -0400

    ibx-1 files to setting up ibx environment

--- ibx_link.bat
+++ ibx_link.bat
@@ -1,6 +1,11 @@
 REM this creates hard links between the git repository and the WebFOCUS install location.
 REM so you don't have to manually move files when developing.
 set wfpath=C:\ibi\WebFOCUS82\webapps\webfocus
+
+REM remove the old links
+rd /S /Q %wfpath%\ibx\testing
 rd /S /Q %wfpath%\ibx
+
+REM make the new links
 MKLINK /D %wfpath%\ibx %CD%\src\main\resources\META-INF\resources\ibx
 MKLINK /D %wfpath%\ibx\testing %CD%\src\test\resources\META-INF\resources\ibx\testing
