e0fc513230f9f10fc304792d7381ff51c27c1f87
Julian Hyman <julian_hyman@ibi.com>
2019-06-18 17:46:47 -0400

    ibx-1 working on setup files

--- ibx_setup.bat
+++ ibx_setup.bat
@@ -1,7 +1,7 @@
 REM get git to checkout the _testing branch into the _testing directory
-git worktree remove -f ./_testing
-md _testing
-git worktree add ./_testing _testing
+git worktree remove -f ../ibx_testing
+md ../ibx_testing
+git worktree add ../ibx_testing _testing
 
 REM creates symbolic links to main ibx directory and testing
 call ibx_link.bat
@@ -9,4 +9,4 @@ call ibx_link.bat
 REM remap _testing testing directory to the install testing directory
 set wfpath=C:\ibi\WebFOCUS82\webapps\webfocus
 rd /S /Q %wfpath%\ibx\testing
-MKLINK /D %wfpath%\ibx\testing %CD%\_testing\src\test\resources\META-INF\resources\ibx\testing
+MKLINK /D %wfpath%\ibx\testing %CD%\..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
