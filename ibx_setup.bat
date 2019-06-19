8e793be528f98150cb1ebe093b034cb04771905f
Julian Hyman <julian_hyman@ibi.com>
2019-06-19 11:13:58 -0400

    ibx-1 updating config files

--- ibx_setup.bat
+++ ibx_setup.bat
@@ -1,12 +1,16 @@
-REM get git to checkout the _testing branch into the _testing directory
-git worktree remove -f ../ibx_testing
-md ../ibx_testing
+
+REM remove _testing worktree
+git worktree remove ../ibx_testing
 git worktree add ../ibx_testing _testing
 
-REM creates symbolic links to main ibx directory and testing
-call ibx_link.bat
-
-REM remap _testing testing directory to the install testing directory
+REM this creates hard links between the git repository and the WebFOCUS install location.
+REM so you don't have to manually move files when developing.
 set wfpath=C:\ibi\WebFOCUS82\webapps\webfocus
-rd /S /Q %wfpath%\ibx\testing
-MKLINK /D %wfpath%\ibx\testing %CD%\..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
+
+REM remove the old links
+rd /S /Q %wfpath%\ibx
+rd /S /Q %wfpath%\ibx_testing
+
+REM make the new links
+MKLINK /D %wfpath%\ibx %CD%\src\main\resources\META-INF\resources\ibx
+MKLINK /D %wfpath%\ibx_testing %CD%..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
