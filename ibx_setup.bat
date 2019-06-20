d41b0f08fceed6f9e7805e0eb94e8c622752a5dc
Julian Hyman <julian_hyman@ibi.com>
2019-06-20 12:27:50 -0400

    ibx-1 working on setup files

--- ibx_setup.bat
+++ ibx_setup.bat
@@ -1,8 +1,8 @@
 REM remove _testing worktree
-git worktree remove ..\ibx_testing
-rd /S /Q ..\ibx_testing
+git worktree remove %CD%\..\ibx_testing
+rd /S /Q %CD%\..\ibx_testing
 
-git worktree add ..\ibx_testing _testing
+git worktree add %CD%\..\ibx_testing _testing
 
 REM this creates hard links between the git repository and the WebFOCUS install location.
 REM so you don't have to manually move files when developing.
@@ -14,4 +14,4 @@ rd /S /Q %wfpath%\ibx_testing
 
 REM make the new links
 MKLINK /D %wfpath%\ibx %CD%\src\main\resources\META-INF\resources\ibx
-MKLINK /D %wfpath%\ibx_testing ..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
+MKLINK /D %wfpath%\ibx_testing %CD%\..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
