0792c9aee0ea720334b109641e774fa48758854d
Julian Hyman <julian_hyman@ibi.com>
2019-06-27 12:45:48 -0400

    ibx-1 working on wfsetup

--- wfsetup.bat
+++ wfsetup.bat
@@ -1,7 +1,7 @@
 REM recreate ibx_testing worktree
-git worktree remove %CD%\..\ibx_testing
-rd /S /Q %CD%\..\ibx_testing
-git worktree add %CD%\..\ibx_testing _testing
+git worktree remove %CD%\..\wf_ibx_testing
+rd /S /Q %CD%\..\wf_ibx_testing
+git worktree add %CD%\..\wf_ibx_testing _testing
 
 REM path to where webfocus is
 set wfpath=C:\ibi\WebFOCUS82\webapps\webfocus
@@ -13,26 +13,27 @@ MKLINK /D %wfpath%\ibx %CD%\src\main\resources\META-INF\resources\ibx
 
 REM make the new ibx_testing link
 rd /S /Q %wfpath%\ibx_testing
-MKLINK /D %wfpath%\ibx_testing %CD%\..\ibx_testing\src\test\resources\META-INF\resources\ibx\testing
+MKLINK /D %wfpath%\ibx_testing %CD%\..\wf_ibx_testing\src\test\resources\META-INF\resources\ibx\testing
 
 REM make the new homepage and ibxtools link
 rename %wfpath%\WEB-INF\lib\webfocus-webjars-ibxtools-HEAD-SNAPSHOT.jar webfocus-webjars-ibxtools-HEAD-SNAPSHOT.jar_
 rd /S /Q %wfpath%\homepage
-git clone http://wfbucket.ibi.com:7990/scm/wp/webfocus-webjars-projects.git ..\ibxtools
-MKLINK /D %wfpath%\homepage %CD%\..\ibxtools\webfocus-webjars-ibxtools\src\main\resources\META-INF\resources\homepage
+git clone http://wfbucket.ibi.com:7990/scm/wp/webfocus-webjars-projects.git ..\wf_ibxtools
+MKLINK /D %wfpath%\homepage %CD%\..\wf_ibxtools\webfocus-webjars-ibxtools\src\main\resources\META-INF\resources\homepage
 rd /S /Q %wfpath%\ibxtools
-MKLINK /D %wfpath%\ibxtools %CD%\..\ibxtools\webfocus-webjars-ibxtools\src\main\resources\META-INF\resources\ibxtools
+MKLINK /D %wfpath%\ibxtools %CD%\..\wf_ibxtools\webfocus-webjars-ibxtools\src\main\resources\META-INF\resources\ibxtools
 
 REM make the new 3rd party resources link
 rename %wfpath%\WEB-INF\lib\webfocus-webjars-3rdparty-resources-HEAD-SNAPSHOT.jar webfocus-webjars-3rdparty-resources-HEAD-SNAPSHOT.jar_
-rd /S /Q %wfpath%\3rdparty_resources
-git clone http://wfbucket.ibi.com:7990/scm/wp/webfocus-webjars-3rdparty-resources.git ..\3rdparty_resources
-MKLINK /D %wfpath%\3rdparty_resources %CD%\..\3rdparty_resources\src\main\resources\META-INF\resources\3rdparty_resources
+rd /S /Q %wfpath%\wf_3rdparty_resources
+git clone http://wfbucket.ibi.com:7990/scm/wp/webfocus-webjars-3rdparty-resources.git ..\wf_3rdparty_resources
+MKLINK /D %wfpath%\3rdparty_resources %CD%\..\wf_3rdparty_resources\src\main\resources\META-INF\resources\3rdparty_resources
 
 REM make the new intl properties/strings resources link
 rename %wfpath%\WEB-INF\lib\webfocus-intl-HEAD-SNAPSHOT.jar webfocus-intl-HEAD-SNAPSHOT.jar_
 git clone http://wfbucket.ibi.com:7990/scm/wp/webfocus-intl.git ..\wf_intl
 mkdir %wfpath%\WEB-INF\classes\com\ibi
+rd /S /Q %wfpath%\WEB-INF\classes\com\ibi\intl
 MKLINK /D %wfpath%\WEB-INF\classes\com\ibi\intl %CD%\..\wf_intl\src\main\resources\com\ibi\intl
 
 REM copy un-minified jquery into install
