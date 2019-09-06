df5493b87f01092ab9e2f3b1c2dc3a580344dca1
Julian Hyman <julian_hyman@ibi.com>
2019-09-06 12:22:18 -0400

    IBX-441: ibxSpinner - working on unformatting value

--- src/main/resources/META-INF/resources/ibx/resources/js/spinner.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/spinner.ibx.js
@@ -14,7 +14,6 @@ $.widget("ibi.ibxSpinner", $.ibi.ibxTextField,
 		"step" :1,
 		"precision":3,
 		"fnFormat":null,
-
 		"btnGroupClass":"ibx-spinner-btn-grp",
 		"btnUpClass":"ibx-spinner-btn-up",
 		"btnUpOptions":
@@ -150,9 +149,9 @@ $.widget("ibi.ibxSpinner", $.ibi.ibxTextField,
 	{
 		var options = this.options;
 		var curVal = options.value;
-		value = Number(String(value).replace(/[^0-9.-]*/g, ""));
 
-		if(isNaN(value))
+		value = Number(String(value).replace(/[^0-9.+\-]*/g, ""));
+		if(value === NaN)
 			value = options.min;
 
 		value = Math.max(value, options.min);
