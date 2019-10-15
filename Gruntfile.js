5f2df164d9df89653698a926c4258f4a8224c338
Yingchun Chen <yingchun_chen@ibi.com>
2019-10-15 12:25:57 -0400

    [CICD-154] dynamic parse the bundle xml to find css and js list

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,127 +1,7 @@
-module.exports = function (grunt) {  
-	var getBanner = function() {
-		var label = 
-			  '/*!\n'
-			+ ' * WebFOCUS <%= pkg.name %> v<%= pkg.version %>\n'
-			+ ' * Copyright 2018-2019 <%= pkg.author %>\n'
-			+ ' * Licensed under <%= pkg.license %>\n'
-			+ ' */';
-		return label;
-	};
-	
-    grunt.initConfig({  
-        pkg: grunt.file.readJSON('package.json'),  
-        cssmin: {  
-        	ibx_css_package: {  
-                options: {                  	
-                    sourceMap: false
-                },  
-                files: {
-                	'./target/classes/META-INF/resources/ibx/resources/css/ibx-all.min.css': [
-                		'./target/classes/META-INF/resources/ibx/resources/css/widget.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/events.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/selection.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/dragdrop.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/collapsible.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/flexbox.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/iframe.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/grid.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/propgrid.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/tree.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/splitter.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/label.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/progress.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/text.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/textarea.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/richedit.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/spinner.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/button.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/slider.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/popup.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/dialog.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/wizard.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/tooltip.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/form.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/tabs.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/accordion.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/menu.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/groupbox.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/gridstack.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/select.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/carousel.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/datepicker.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/sortable.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/colorpicker.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/diagram.ibx.css',
-                		'./target/classes/META-INF/resources/ibx/resources/css/navmap.ibx.css'
-                	]
-                }            
-            }  
-        },  
-        uglify: {  
-            options: {  
-                compress: true
-            },  
-            ibx_js_package: {
-            	options: {  
-            		banner: getBanner(),
-                    sourceMap: false
-                },
-            	files: {
-            		'./target/classes/META-INF/resources/ibx/resources/js/ibx-all.min.js': [  
-            			//EXTERNAL ASSETS
-	                	'./target/classes/META-INF/resources/ibx/resources/etc/resizepolyfill/ResizeSensor.js',
-	            		'./target/classes/META-INF/resources/ibx/resources/etc/scrollwidthpolyfill/scrollwidthpolyfill.js',
-	            		'./target/classes/META-INF/resources/ibx/resources/etc/gridstack/underscore.js',
-	            		'./target/classes/META-INF/resources/ibx/resources/etc/gridstack/gridstack.js',
-	            		'./target/classes/META-INF/resources/ibx/resources/etc/jquery-minicolors/jquery.minicolors.js',
-	            		//IBX ASSETS
-		                './target/classes/META-INF/resources/ibx/resources/js/events.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/selection.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/dragdrop.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/mutationobserver.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/shell.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/widget.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/commands.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/autoscroll.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/sortable.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/collapsible.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/iframe.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/flexbox.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/grid.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/propgrid.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/splitter.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/label.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/tree.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/progress.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/text.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/textarea.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/richedit.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/spinner.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/button.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/slider.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/popup.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/dialog.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/tooltip.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/radiogroup.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/form.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/accordion.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/menu.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/groupbox.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/gridstack.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/select.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/carousel.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/tabs.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/wizard.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/datepicker.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/colorpicker.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/diagram.ibx.js',
-		        		'./target/classes/META-INF/resources/ibx/resources/js/navmap.ibx.js'
-	                ]
-            	}
-            }  
-        },
-        replace: {
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg : grunt.file.readJSON('package.json'),
+		replace: {
         	ibx_css_target: {
         		src: ['./target/classes/META-INF/resources/ibx/resources/ibx_resource_bundle.xml'],
         		overwrite: true,
@@ -143,11 +23,310 @@ module.exports = function (grunt) {
 				]
         	}
         }
-    });      
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-text-replace');
-    
-    //Default task.
-    grunt.registerTask('default', ['uglify', 'cssmin', 'replace']);  
+	});
+
+	var log = function(data) {
+		console.log(data);
+	};
+	
+	var getTargetBundlePath = function(file) {
+		//./src/main/resources/META-INF/resources/ibxtools/shared_resources/shared_resource_bundle.xml
+		var srcRootPrefix = 'src/main/resources/META-INF/resources/';
+		var destRootPrefix = './target/classes/META-INF/resources/';
+		var index = file.indexOf(srcRootPrefix);
+		var path = destRootPrefix + file.substring(index + srcRootPrefix.length);
+		return path.trim();
+	};
+	
+	var getTargetBundleFolderPath = function(file) {
+		//./src/main/resources/META-INF/resources/ibxtools/shared_resources/shared_resource_bundle.xml
+		var path = getTargetBundlePath(file);
+		var index = path.lastIndexOf('/');
+		return path.substring(0, index + 1);
+	};
+	
+	var isValidPath = function(file) {
+		//<style-file src="//js.arcgis.com/4.9/esri/css/main.css"/>
+		//<script-file src="//js.arcgis.com/4.9/"/>
+		if(file.startsWith('http://') || 
+				file.startsWith('https://') ||
+				file.startsWith('//')) {
+			return false;
+		}
+		return true;
+	};
+	
+	var getTargetPath = function(file, path, loadContext) {
+		//<style-file src="insight.css"/>
+		//<style-file src="./css/mapping.css"/>
+		//<style-file src="{context}/ibxtools/shared_resources/css/outputviewer.css"/>
+		
+		//<script-file src="./resources/js/baseChartPane.wfc.js"/>
+		//<script-file src="{context}/ibxtools/explore/resources/js/filegrid.js" />
+		//<script-file src="./js/home.js" loadContext="bundle"/>	
+		//<script-file src="{context}/ibxtools/shared_resources/js/ibfs.js" loadContext="bundle"/>
+		//<script-file src="//js.arcgis.com/4.9/"/>
+		if(!isValidPath(file)) {
+			log('warning: ' + file);
+			return null;
+		}
+		
+		var destRootPrefix = './target/classes/META-INF/resources/';
+		var dot = '.';
+		var twoDot = '..';
+		var context = '{context}';
+		if(file.startsWith(twoDot)) {
+			//do not support
+			return path + file;
+		}
+		else if(file.startsWith(dot)) {
+			return path + file.substring(file.indexOf('/') + 1);
+		}
+		else if(file.startsWith(context)) {
+			return destRootPrefix + file.substring(file.indexOf('/') + 1);
+		}
+		
+		return path + file;
+	};
+	
+	var getCSSTargetPath = function(file, path) {
+		return getTargetPath(file, path, null);
+	};
+	
+	var getJSTargetPath = function(file, path, loadContext) {
+		return getTargetPath(file, path, loadContext);
+	};
+	
+	var parseBundleXML = function(file) {
+		var path = getTargetBundleFolderPath(file);
+		var cssData = {
+			'bundle': getTargetBundlePath(file),
+			'bundleSource': file,
+			'path': path,
+			'minfile': '',
+			'minfileSource': '',
+			'files': []
+		};
+		var jsData = {
+			'bundle': getTargetBundlePath(file),
+			'bundleSource': file,
+			'path': path,
+			'minfile': '',
+			'minfileSource': '',
+			'files': []	
+		};
+		
+		var cssStartPrefix = '<!--IBX CSS ASSETS';
+		var jsStartPrefix = '<!--EXTERNAL JS ASSETS';
+		var endPrefix = '<!--inline ';
+		var commentEnd = '-->';
+		var doubleQuote = '\"';
+		var keyLoadContext = 'loadContext';
+		
+		var cssStart = false;
+		var jsStart = false;
+			
+		var data = grunt.file.read(file);
+		var remaining = data;
+		var index = remaining.indexOf('\n');
+		
+		while (index > -1) {
+			var line = remaining.substring(0, index);
+			line = line.trim();
+			remaining = remaining.substring(index + 1);
+			index = remaining.indexOf('\n');			
+			if(line.length == 0) {
+				continue;
+			}
+			if(line.startsWith(cssStartPrefix)) {
+				cssStart = true;
+				var end = line.lastIndexOf(commentEnd);
+				var minfile = "";
+				if(file.endsWith('ibx_resource_bundle.xml')) {
+					minfile = "./css/ibx-all.min.css";
+				}
+				else {
+					minfile = line.substring(cssStartPrefix.length, end).trim();
+				}
+				cssData.minfile = getCSSTargetPath(minfile, path);
+				cssData.minfileSource = minfile;
+			}
+			else if(line.startsWith(jsStartPrefix)) {
+				jsStart = true;
+				var end = line.lastIndexOf(commentEnd);
+				var minfile = "";
+				if(file.endsWith('ibx_resource_bundle.xml')) {
+					minfile = "./js/ibx-all.min.js";
+				}
+				else {
+					minfile = line.substring(jsStartPrefix.length, end).trim();
+				}
+				jsData.minfile = getJSTargetPath(minfile, path, null);
+				jsData.minfileSource = minfile;
+			}
+			else if(line.startsWith(endPrefix)) {
+				if(cssStart) {
+					cssStart = false;
+				}
+				if(jsStart) {
+					jsStart = false;
+				}
+			}
+			else if(cssStart || jsStart) {
+				if(line.indexOf(" src") == -1) {
+					continue;
+				}
+				var start = line.indexOf(doubleQuote);
+				var end = line.indexOf(doubleQuote, start + doubleQuote.length);
+				if(start == -1 || end == -1) {
+					continue;
+				}
+				var src = line.substring(start + doubleQuote.length, end).trim();
+				if(cssStart) {
+					src = getCSSTargetPath(src, path);
+					if(src != null) {
+						cssData.files.push(src);
+					}
+				}
+				else if(jsStart) {
+					start = line.lastIndexOf(keyLoadContext);
+					var loadContext = null;
+					if(start != -1) {
+						start = line.indexOf(doubleQuote, start + keyLoadContext.length);
+						end = line.indexOf(doubleQuote, start + doubleQuote.length);
+						loadContext = line.substring(start + doubleQuote.length, end);
+						//log('loadContext: ' + loadContext);
+					}
+					src = getJSTargetPath(src, path, loadContext);
+					if(src != null) {
+						jsData.files.push(src);
+					}	
+				}
+			}
+		}
+		
+		var result = {
+				'cssData': cssData,
+				'jsData': jsData
+				};
+		return result;
+	};
+	
+	var getBanner = function() {
+		var label = 
+			  '/*!\n'
+			+ ' * WebFOCUS <%= pkg.name %> v<%= pkg.version %>\n'
+			+ ' * Copyright 2018-2019 <%= pkg.author %>\n'
+			+ ' * Licensed under <%= pkg.license %>\n'
+			+ ' */';
+		return label;
+	};
+
+	var getCSSMinTasks = function(list) {
+		var cssTasks = {};
+		for (var i = 0; i < list.length; i++) {
+			var cssData = list[i];
+			var task = {
+					options: {
+	                    sourceMap: false
+	                },
+					files: {}
+			};
+			task.files[cssData.minfile] = cssData.files;
+			cssTasks[cssData.minfile] = task;
+		}
+		return cssTasks;
+	};
+
+	var getJSMinTasks = function(list) {
+		var jsTasks = {};
+		for (var i = 0; i < list.length; i++) {
+			var jsData = list[i];
+			var label = getBanner();
+			var task = {
+					options: {
+						banner: label,
+						compress: true,
+	                    sourceMap: false
+	                },
+					files: {}
+			};
+			task.files[jsData.minfile] = jsData.files;
+			jsTasks[jsData.minfile] = task;
+		}
+		return jsTasks;
+	};
+	
+	var getMinfileBasedOnBundle = function(data) {
+		var src = data.minfileSource;
+		if(src.startsWith('{context}')) {
+			return src;
+		}
+		
+		var destRootPrefix = './target/classes/META-INF/resources/';
+		var index = data.path.indexOf(destRootPrefix);
+		
+		if(src.startsWith('./')) {
+			src = '{context}/' + data.path.substring(index + destRootPrefix.length) + src.substring(2);
+		}
+		else {
+			src = '{context}/' + data.path.substring(index + destRootPrefix.length) + src;
+		}
+		return src;
+	};
+	
+	var getReplaceTasks = function(list) {
+		var replaceTasks = {};
+		for (var i = 0; i < list.length; i++) {
+			var data = list[i];
+			var fromRegex = '';
+			var toString = '';
+			if(data.minfile.endsWith('.css')) {
+				fromRegex = new RegExp('<!-- build:css (?:\r|\n|.)+<!-- endbuild:css -->', 'm');
+				toString = '<style-file src="' + getMinfileBasedOnBundle(data) + '"/>';
+				flag = 'Target: CSS';
+			}
+			else {
+				fromRegex = new RegExp('<!-- build:js (?:\r|\n|.)+<!-- endbuild:js -->', 'm');
+				toString = '<script-file src="' + getMinfileBasedOnBundle(data) + '"/>';
+			}
+			var task = {
+					src: [data.bundle],
+					overwrite: true,
+					replacements: [{from: fromRegex, to: toString}]
+			};
+			replaceTasks['Target-' + i] = task;
+		}
+		return replaceTasks;
+	};
+
+	var bundleXmlFiles = grunt.file
+			.expand('./src/main/resources/META-INF/resources/**/ibx_resource_bundle.xml');
+	
+	var cssDataList = [];
+	var jsDataList = [];
+	bundleXmlFiles.forEach(function(file) {
+		log('bundle: ' + file);
+		var data = parseBundleXML(file);
+		if(data.cssData.minfile != null && data.cssData.files.length > 0) {
+			log('\tCSS min: ' + data.cssData.minfile);
+			cssDataList.push(data.cssData);
+		}
+		if(data.jsData.minfile != null && data.jsData.files.length > 0) {
+			log('\tJS min: ' + data.jsData.minfile);
+			jsDataList.push(data.jsData);
+		}
+	});
+	
+	grunt.config('cssmin', getCSSMinTasks(cssDataList));
+	grunt.config('uglify', getJSMinTasks(jsDataList));
+	//grunt.config('replace', getReplaceTasks(cssDataList.concat(jsDataList)));
+
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-text-replace');
+
+	// Default task.
+	grunt.registerTask('default', ['cssmin', 'uglify', 'replace']);
+
 };
