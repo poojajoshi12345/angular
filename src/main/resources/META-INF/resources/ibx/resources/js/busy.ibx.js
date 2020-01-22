1e62bc420d37cd5c9af1b173075dfbf823d316e8
Julian <julian_hyman@ibi.com>
2020-01-22 15:45:37 -0500

    IBX-486: ibxBusy

--- src/main/resources/META-INF/resources/ibx/resources/js/busy.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/busy.ibx.js
@@ -2,41 +2,76 @@
 
 function ibxBusy(config)
 {
-	this.config = config;
+	this._config =
+	{
+		"template":
+			"<div class='ibx-busy-img'></div>"+
+			"<div class='ibx-busy-msg-box'>"+
+				"<div class='ibx-busy-msg'></div>"+
+			"</div>"+
+			"<div class='ibx-busy-btn-box'>"+
+			"</div>"+
+			"",
+		"image":"./loader_small.gif",
+		"message":"Loading..."
+	}
 	this._element = document.createElement("div");
 	this._element.classList.add("ibx-busy-container");
-	var template = config.template || 
-		"<img class='ibx-busy-img' src='" +  config.image + "'/>"+
-		"<div class='ibx-busy-msg-box'>"+
-			"<div class='ibx-busy-msg'>" + config.message + "</div>"+
-		"</div>"+
-		"";
-	this._element.innerHTML = template;
-	var css = "";
+
+	this.init = function(config)
+	{
+		config = config || {};
+		this._config.template = config.template || this._config.template;
+		this._element.innerHTML = this._config.template;
+
+		this._config.image = config.image || this._config.image;
+		var elImage = this._element.querySelector(".ibx-busy-img");
+		elImage.style.backgroundImage = "url('" + this._config.image + "')";
+		
+		this._config.message = config.message || this._config.message;
+		var elMsg = this._element.querySelector(".ibx-busy-msg");
+		elMsg.innerText = this._config.message;
+	}
+	this.init(config);
+
 	this.isVisible = function()
 	{
 		return this._element.parentElement ? true : false;
 	}
-	this.show = function(bShow, elParent)
+	this.show = function(bShow, elParent, config)
 	{
+		//overloaded so elParent can be config and elParent defaults to documentElement.
+		config = (elParent instanceof HTMLElement) ? config : elParent;
+		elParent = (elParent instanceof HTMLElement) ? elParent : document.documentElement;
+		this.init(config);
+
 		if(bShow)
 		{
-			elParent = elParent || document.documentElement;
 			elParent.classList.add("ibx-busy-parent");
 			elParent.appendChild(this._element);
 		}
 		else
+		if(this._element.parentElement)
 		{
 			this._element.parentElement.classList.remove("ibx-busy-parent");
 			this._element.parentElement.removeChild(this._element);
 		}
 	}
+	this.message = function(msg)
+	{
+		var elMsg = this._element.querySelector(".ibx-busy-msg");
+		if(msg === undefined)
+			return elMsg.innerText;
+		elMsg.innerText = msg;
+		return elMsg.innerText;
+	}
 }
+ibxBusy.busy = new ibxBusy();
 
 (function()
 {
 	var event = document.createEvent("CustomEvent");
-	event.initCustomEvent("ibx_busyloaded", false, false, null);
+	event.initCustomEvent("ibxbusyready", false, false, null);
 	window.dispatchEvent(event);
 })()
 
