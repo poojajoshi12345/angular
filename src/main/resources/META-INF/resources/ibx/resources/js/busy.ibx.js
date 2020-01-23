67b57e5a874f8ef9caa6298334af1f53592764c2
Julian <julian_hyman@ibi.com>
2020-01-23 14:10:39 -0500

    IBX-486: ibxBusy - added buttons

--- src/main/resources/META-INF/resources/ibx/resources/js/busy.ibx.js
+++ src/main/resources/META-INF/resources/ibx/resources/js/busy.ibx.js
@@ -14,8 +14,9 @@ function ibxBusy(config)
 			"",
 		"css":null,
 		"image":"",
-		"message":"Loading..."
-	}
+		"message":"Loading...",
+		"buttons":[]
+	};
 	this._element = document.createElement("div");
 	this._element.classList.add("ibx-busy-container");
 
@@ -31,29 +32,48 @@ function ibxBusy(config)
 		this._css.classList.add("ibx-busy-styles");
 		this._css.innerText = this._config.css;
 
-		this._config.image = config.image || this._config.image;
 		var elImage = this._element.querySelector(".ibx-busy-img");
-		this._config.image ? elImage.style.backgroundImage = "url('" + this._config.image + "')" : null;
-		
-		this._config.message = config.message || this._config.message;
+		if(elImage)
+		{
+			this._config.image = config.image || this._config.image;
+			this._config.image ? elImage.style.backgroundImage = "url('" + this._config.image + "')" : null;
+		}
+
 		var elMsg = this._element.querySelector(".ibx-busy-msg");
-		elMsg.innerText = this._config.message;
-	}
+		if(elMsg)
+		{
+			this._config.message = config.message || this._config.message;
+			elMsg.innerText = this._config.message;
+		}
+
+		var btnBox = this._element.querySelector(".ibx-busy-btn-box");
+		if(btnBox)
+		{
+			this._config.buttons = config.buttons || this._config.buttons;
+			btnBox.innerHTML = this._config.buttons.join(" ");
+		}
+	};
 	this.init(config);
 
 	this.isVisible = function()
 	{
 		return this._element.parentElement ? true : false;
-	}
+	};
+
+	this.getElement = function()
+	{
+		return this._element;
+	};
+
 	this.show = function(bShow, elParent, config)
 	{
 		//overloaded so elParent can be config and elParent defaults to documentElement.
 		config = (elParent instanceof HTMLElement) ? config : elParent;
 		elParent = (elParent instanceof HTMLElement) ? elParent : document.documentElement;
-		this.init(config);
 
 		if(bShow)
 		{
+			this.init(config);
 			document.head.appendChild(this._css);
 			elParent.classList.add("ibx-busy-parent");
 			elParent.appendChild(this._element);
@@ -65,7 +85,9 @@ function ibxBusy(config)
 			this._element.parentElement.classList.remove("ibx-busy-parent");
 			this._element.parentElement.removeChild(this._element);
 		}
-	}
+		return this._element;
+	};
+
 	this.message = function(msg)
 	{
 		var elMsg = this._element.querySelector(".ibx-busy-msg");
@@ -73,7 +95,7 @@ function ibxBusy(config)
 			return elMsg.innerText;
 		elMsg.innerText = msg;
 		return elMsg.innerText;
-	}
+	};
 }
 ibxBusy.busy = new ibxBusy();
 
